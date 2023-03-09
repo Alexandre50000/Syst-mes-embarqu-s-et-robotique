c4e65a31df12d720625b02c07b6c1a6dcc316e40
Alexandre <alexandre.airesviegas@efl.ch>
2023-03-09 10:34:26 +0100

    Imported files

---
+++ e-puck2_main-processor/.dep/flash.o.d
@@ -0,0 +1,28 @@
+build/obj/flash.o: src/flash/flash.c \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\lib\gcc\arm-none-eabi\7.2.1\include\stdint.h \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\stdint.h \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\machine\_default_types.h \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\sys\features.h \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\_newlib_version.h \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\sys\_intsup.h \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\sys\_stdint.h \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\lib\gcc\arm-none-eabi\7.2.1\include\stddef.h \
+ src/flash/flash.h
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\lib\gcc\arm-none-eabi\7.2.1\include\stdint.h:
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\stdint.h:
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\machine\_default_types.h:
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\sys\features.h:
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\_newlib_version.h:
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\sys\_intsup.h:
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\sys\_stdint.h:
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\lib\gcc\arm-none-eabi\7.2.1\include\stddef.h:
+
+src/flash/flash.h:
--- src/timer.h
+++ src/timer.h
@@ -2,6 +2,6 @@
 #define TIMER_H
 
 void timer7_start(void);
-
-
+void  timer4_start(void);
+void TIM4_IRQHandler(void);
 #endif /* TIMER_H */
