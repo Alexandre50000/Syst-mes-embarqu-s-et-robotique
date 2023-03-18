8cfedecf7270b5f08bae2aca58a8f3dbaea8e781
Alexandre <alexandre.airesviegas@efl.ch>
2023-03-18 21:13:55 +0100

    First commit

---
+++ src/motor.d
@@ -0,0 +1,93 @@
+motor.o: motor.c \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\stdlib.h \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\machine\ieeefp.h \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\_ansi.h \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\newlib.h \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\_newlib_version.h \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\sys\config.h \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\sys\features.h \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\lib\gcc\arm-none-eabi\7.2.1\include\stddef.h \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\sys\reent.h \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\_ansi.h \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\sys\_types.h \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\machine\_types.h \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\machine\_default_types.h \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\sys\lock.h \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\sys\cdefs.h \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\machine\stdlib.h \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\alloca.h \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\lib\gcc\arm-none-eabi\7.2.1\include\stdint.h \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\stdint.h \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\sys\_intsup.h \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\sys\_stdint.h \
+ ../ST/stm32f4xx.h ../ST/stm32f407xx.h ../ST/core_cm4.h \
+ ../ST/core_cmInstr.h ../ST/cmsis_gcc.h ../ST/core_cmFunc.h \
+ ../ST/core_cmSimd.h ../ST/system_stm32f4xx.h gpio.h ../ST/stm32f407xx.h \
+ c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\lib\gcc\arm-none-eabi\7.2.1\include\stdbool.h \
+ motor.h
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\stdlib.h:
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\machine\ieeefp.h:
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\_ansi.h:
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\newlib.h:
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\_newlib_version.h:
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\sys\config.h:
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\sys\features.h:
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\lib\gcc\arm-none-eabi\7.2.1\include\stddef.h:
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\sys\reent.h:
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\_ansi.h:
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\sys\_types.h:
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\machine\_types.h:
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\machine\_default_types.h:
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\sys\lock.h:
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\sys\cdefs.h:
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\machine\stdlib.h:
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\alloca.h:
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\lib\gcc\arm-none-eabi\7.2.1\include\stdint.h:
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\stdint.h:
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\sys\_intsup.h:
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\arm-none-eabi\include\sys\_stdint.h:
+
+../ST/stm32f4xx.h:
+
+../ST/stm32f407xx.h:
+
+../ST/core_cm4.h:
+
+../ST/core_cmInstr.h:
+
+../ST/cmsis_gcc.h:
+
+../ST/core_cmFunc.h:
+
+../ST/core_cmSimd.h:
+
+../ST/system_stm32f4xx.h:
+
+gpio.h:
+
+../ST/stm32f407xx.h:
+
+c:\users\alexa\appdata\roaming\epuck2tools\gcc-arm-none-eabi-7-2017-q4-major\lib\gcc\arm-none-eabi\7.2.1\include\stdbool.h:
+
+motor.h:
