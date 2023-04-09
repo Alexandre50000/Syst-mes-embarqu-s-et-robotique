90d79f26041cd70454095efa955e809636971055
Alexandre <alexandre.airesviegas@efl.ch>
2023-04-09 10:34:24 +0200

    Task 7 done, TP5 done

--- src/audio_processing.c
+++ src/audio_processing.c
@@ -11,6 +11,24 @@
 #include "fft.h"
 #include "communications.h"
 
+#define MIN_FREQ		10	//we don't analyze before this index to not use resources for nothing
+#define FREQ_FORWARD	16	//250Hz
+#define FREQ_LEFT		19	//296Hz
+#define FREQ_RIGHT		23	//359HZ
+#define FREQ_BACKWARD	26	//406Hz
+#define MAX_FREQ		30	//we don't analyze after this index to not use resources for nothing
+
+#define FREQ_FORWARD_L		(FREQ_FORWARD-1)
+#define FREQ_FORWARD_H		(FREQ_FORWARD+1)
+#define FREQ_LEFT_L			(FREQ_LEFT-1)
+#define FREQ_LEFT_H			(FREQ_LEFT+1)
+#define FREQ_RIGHT_L		(FREQ_RIGHT-1)
+#define FREQ_RIGHT_H		(FREQ_RIGHT+1)
+#define FREQ_BACKWARD_L		(FREQ_BACKWARD-1)
+#define FREQ_BACKWARD_H		(FREQ_BACKWARD+1)
+
+#define MIN_VALUE_THRESHOLD	0 
+
 //semaphore
 static BSEMAPHORE_DECL(sendToComputer_sem, TRUE);
 
@@ -25,6 +43,43 @@ static float micRight_output[FFT_SIZE];
 static float micFront_output[FFT_SIZE];
 static float micBack_output[FFT_SIZE];
 
+
+void sound_move(float* mic_out){
+	uint16_t peak = MIN_VALUE_THRESHOLD;
+	uint8_t beep = MIN_FREQ;
+	for(uint8_t i = MIN_FREQ; i < MAX_FREQ; ++i){
+		if (mic_out[i] > peak){
+			beep = i;
+			peak = mic_out[i];
+		}
+	}
+
+	//go forward
+	if(beep >= FREQ_FORWARD_L && beep <= FREQ_FORWARD_H){
+		left_motor_set_speed(600);
+		right_motor_set_speed(600);
+	}
+	//turn left
+	else if(beep >= FREQ_LEFT_L && beep <= FREQ_LEFT_H){
+		left_motor_set_speed(-600);
+		right_motor_set_speed(600);
+	}
+	//turn right
+	else if(beep >= FREQ_RIGHT_L && beep <= FREQ_RIGHT_H){
+		left_motor_set_speed(600);
+		right_motor_set_speed(-600);
+	}
+	//go backward
+	else if(beep>= FREQ_BACKWARD_L && beep <= FREQ_BACKWARD_H){
+		left_motor_set_speed(-600);
+		right_motor_set_speed(-600);
+	}
+	else{
+		left_motor_set_speed(0);
+		right_motor_set_speed(0);
+	}
+}
+
 /*
 *	Callback called when the demodulation of the four microphones is done.
 *	We get 160 samples per mic every 10ms (16kHz)
@@ -35,10 +90,49 @@ static float micBack_output[FFT_SIZE];
 *	uint16_t num_samples	Tells how many data we get in total (should always be 640)
 */
 void processAudioData(int16_t *data, uint16_t num_samples){
-	for(uint16_t i=0; i < num_samples/4 ; ++i) {
+	static uint16_t total_samples = 0;
+	static uint8_t wait_send = 0;
+	uint16_t i = 0;
+	do{
+
+		micRight_cmplx_input[total_samples] = data[i];
+		micRight_cmplx_input[total_samples+1] = 0;
+
+		micLeft_cmplx_input[total_samples] = data[i+1];
+		micLeft_cmplx_input[total_samples+1] = 0;
+
+		micFront_cmplx_input[total_samples] = data[i+2];
+		micFront_cmplx_input[total_samples+1] = 0;
+
+		micBack_cmplx_input[total_samples] = data[i+3];
+		micBack_cmplx_input[total_samples+1] = 0;
+		
+		i += 4;
+		total_samples += 2;
+		
+	}while(total_samples < 2*FFT_SIZE && i < num_samples);
+
+	if(total_samples >= 2*FFT_SIZE){
+		total_samples = 0;
+		// Compute FFT
+		doFFT_optimized(FFT_SIZE, micLeft_cmplx_input);
+		doFFT_optimized(FFT_SIZE, micRight_cmplx_input);
+		doFFT_optimized(FFT_SIZE, micFront_cmplx_input);
+		doFFT_optimized(FFT_SIZE, micBack_cmplx_input);
+		// Compute mag
+		arm_cmplx_mag_f32(micRight_cmplx_input, micRight_output, FFT_SIZE);
+		arm_cmplx_mag_f32(micLeft_cmplx_input, micLeft_output, FFT_SIZE);
+		arm_cmplx_mag_f32(micFront_cmplx_input, micFront_output, FFT_SIZE);
+		arm_cmplx_mag_f32(micBack_cmplx_input, micBack_output, FFT_SIZE);
+
+		++wait_send;
+		if(wait_send > 8){
+			wait_send = 0;
+			chBSemSignal(&sendToComputer_sem);
+		}
+		sound_move(micLeft_output);
 		
 	}
-
 	/*
 	*
 	*	We get 160 samples per mic every 10ms
