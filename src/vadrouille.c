a3a11c10fd7871be8d378f47b60113a6aea67187
Alexandre <alexandre.airesviegas@efl.ch>
2023-05-13 10:22:31 +0200

    - Better Patrol

--- src/vadrouille.c
+++ src/vadrouille.c
@@ -3,6 +3,7 @@
 #include <ch.h>
 #include <hal.h>
 #include <motors.h>
+#include <sensors/VL53L0X/VL53L0X.h>
 
 
 #include "vadrouille.h"
@@ -15,24 +16,40 @@
 
 // Fonction de déplacement aléatoire
 void randomMove(void) {
-    int leftSpeed, rightSpeed;
-  
-    // Génération de vitesses aléatoires pour les moteurs
-    leftSpeed = rand() % (2 * MAX_SPEED) - MAX_SPEED;
-    rightSpeed = rand() % (2 * MAX_SPEED) - MAX_SPEED;
 
     // Appliquer les vitesses aux moteurs de mouvement
-    left_motor_set_speed(leftSpeed);
-    right_motor_set_speed(rightSpeed);
+    left_motor_set_speed(MAX_SPEED);
+    right_motor_set_speed(MAX_SPEED);
+    
+    chThdSleepMilliseconds(500);
 
-    // Attendre une durée aléatoire pour la rotation
-    int turnTime = rand() % TURN_TIME;
-    chThdSleepMilliseconds(turnTime);
+    left_motor_set_speed(0);
+    right_motor_set_speed(0);
+
+    chThdSleepMilliseconds(500);
+    
+
+    // Attendre une direction aléatoire pour la rotation
+    int8_t turndir = rand() % 2;
+    if(turndir){
+        left_motor_set_speed(-MAX_SPEED);
+        right_motor_set_speed(MAX_SPEED);
+
+    }
+    else{
+        left_motor_set_speed(MAX_SPEED);
+        right_motor_set_speed(-MAX_SPEED);
+
+    }
+    chThdSleepMicroseconds(320200);
 
     // Arrêter les moteurs après la rotation
     left_motor_set_speed(0);
     right_motor_set_speed(0);
+    
+    chThdSleepMilliseconds(500);
 }
+
 
 static THD_WORKING_AREA(waVadrouille, 256);
 static THD_FUNCTION(Vadrouille, arg) {
